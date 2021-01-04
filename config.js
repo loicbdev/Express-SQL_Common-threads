9b7f53367e4558b8c93f105d38ff57db3adbc09f
loicbdev <loicbodolec@msn.com>
2021-01-04 22:01:29 +0100

    ajout env.example

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
+    port: process.env.PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
